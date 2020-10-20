119cc78739844712b5713f3be4dd41ef4b2cdaae
Hong Ooi <hongooi@microsoft.com>
2020-10-21 07:04:42 +1100

    don't use R6 if not needed

--- R/table_batch_request.R
+++ R/table_batch_request.R
@@ -1,69 +1,59 @@
-BatchOperation <- R6::R6Class("BatchOperation",
-
-public=list(
-    endpoint=NULL,
-    path=NULL,
-    options=NULL,
-    headers=NULL,
-    method=NULL,
-    body=NULL,
-
-    initialize=function(endpoint, path, options=list(), headers=list(), body=NULL,
-        metadata=c("none", "minimal", "full"),
-        http_verb=c("GET", "PUT", "POST", "PATCH", "DELETE", "HEAD"))
+create_batch_operation <- function(endpoint, path, options=list(), headers=list(), body=NULL,
+    metadata=c("none", "minimal", "full"), http_verb=c("GET", "PUT", "POST", "PATCH", "DELETE", "HEAD"))
+{
+    accept <- if(!is.null(metadata))
     {
-        accept <- if(!is.null(metadata))
-        {
-            metadata <- match.arg(metadata)
-            switch(match.arg(metadata),
-                "none"="application/json;odata=nometadata",
-                "minimal"="application/json;odata=minimalmetadata",
-                "full"="application/json;odata=fullmetadata")
-        }
-        else NULL
+        metadata <- match.arg(metadata)
+        switch(match.arg(metadata),
+            "none"="application/json;odata=nometadata",
+            "minimal"="application/json;odata=minimalmetadata",
+            "full"="application/json;odata=fullmetadata")
+    }
+    else NULL
 
-        self$endpoint <- endpoint
-        self$path <- path
-        self$options <- options
-        self$headers <- utils::modifyList(headers, list(Accept=accept, DataServiceVersion="3.0;NetFx"))
-        self$method <- http_verb
-        self$body <- body
-    },
+    obj <- list()
+    obj$endpoint <- endpoint
+    obj$path <- path
+    obj$options <- options
+    obj$headers <- utils::modifyList(headers, list(Accept=accept, DataServiceVersion="3.0;NetFx"))
+    obj$method <- http_verb
+    obj$body <- body
+    structure(obj, class="batch_operation")
+}
 
-    serialize=function()
-    {
-        url <- httr::parse_url(self$endpoint$url)
-        url$path <- self$path
-        url$query <- self$options
+
+serialize_batch_operation <- function(object)
+{
+    UseMethod("serialize_batch_operation")
+}
+
+
+serialize_batch_operation.batch_operation <- function(object)
+{
+        url <- httr::parse_url(object$endpoint$url)
+        url$path <- object$path
+        url$query <- object$options
 
         preamble <- c(
             "Content-Type: application/http",
             "Content-Transfer-Encoding: binary",
             "",
-            paste(self$method, httr::build_url(url), "HTTP/1.1"),
-            paste0(names(self$headers), ": ", self$headers),
-            if(!is.null(self$body)) "Content-Type: application/json"
+            paste(object$method, httr::build_url(url), "HTTP/1.1"),
+            paste0(names(object$headers), ": ", object$headers),
+            if(!is.null(object$body)) "Content-Type: application/json"
         )
 
-        if(is.null(self$body))
+        if(is.null(object$body))
             preamble
-        else if(!is.character(self$body))
+        else if(!is.character(object$body))
         {
-            body <- jsonlite::toJSON(self$body, auto_unbox=TRUE, null="null")
+            body <- jsonlite::toJSON(object$body, auto_unbox=TRUE, null="null")
             # special-case treatment for 1-row dataframes
-            if(is.data.frame(self$body) && nrow(self$body) == 1)
+            if(is.data.frame(object$body) && nrow(object$body) == 1)
                 body <- substr(body, 2, nchar(body) - 1)
             c(preamble, "", body)
         }
-        else c(preamble, "", self$body)
-    }
-))
-
-
-create_batch_operation <- function(endpoint, path, options=list(), headers=list(), body=NULL,
-    metadata=c("none", "minimal", "full"), http_verb=c("GET", "PUT", "POST", "PATCH", "DELETE", "HEAD"))
-{
-    BatchOperation$new(endpoint, path, options, headers, body, metadata, http_verb)
+        else c(preamble, "", object$body)
 }
 
 
@@ -84,8 +74,10 @@ send_batch_request <- function(endpoint, operations, batch_status_handler=c("war
         paste0("--", changeset_bound, "--"),
         paste0("--", batch_bound, "--")
     )
-    serialized <- lapply(operations, function(op) c(paste0("--", changeset_bound), op$serialize()))
+    serialized <- lapply(operations, function(op) c(paste0("--", changeset_bound), serialize_batch_operation(op)))
     body <- paste0(c(batch_preamble, unlist(serialized), batch_postscript), collapse="\n")
+    if(nchar(body) > 4194304)
+        stop("Batch request too large, must be 4MB or less")
     headers$`Content-Length` <- nchar(body)
 
     res <- call_table_endpoint(endpoint, "$batch", headers=headers, body=body, encode="raw",
@@ -153,6 +145,14 @@ process_operation_response <- function(response, handler)
     obj <- list(status=status, headers=headers, body=body)
     class(obj) <- "batch_operation_response"
     obj
+}
+
+
+#' @export
+print.batch_operation <- function(x, ...)
+{
+    cat("<Table storage batch operation>\n")
+    invisible(x)
 }
 
 #' @export
